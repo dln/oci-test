30b564ae487a4067db0b1726785d451ef8cb8947
Daniel Lundin <dln@arity.se>
2024-03-15 22:42:38 +0100

    Run container (no config generation yet)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use oci_distribution::manifest;
+use anyhow::{Context, Result};
+use flate2::read::GzDecoder;
+use libcontainer::container::builder::ContainerBuilder;
+use libcontainer::oci_spec::runtime::Spec;
+use libcontainer::syscall::syscall::SyscallType;
+use libcontainer::workload::{Executor, ExecutorError, ExecutorValidationError};
+use nix::{
+    sys::{
+        signal::{self, kill},
+        signalfd::SigSet,
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    unistd::Pid,
+};
 use oci_distribution::client::*;
+use oci_distribution::manifest;
 use oci_distribution::secrets::RegistryAuth;
 use oci_distribution::Reference;
-use tracing_subscriber::prelude::*;
 use std::io::Cursor;
-use flate2::read::GzDecoder;
 use tar::Archive;
+use tracing_subscriber::prelude::*;
+
+#[derive(Clone)]
+pub struct MyExecutor {}
+
+impl Executor for MyExecutor {
+    fn exec(&self, spec: &Spec) -> Result<(), ExecutorError> {
+        libcontainer::workload::default::get_executor().exec(spec)
+    }
+
+    fn validate(&self, spec: &Spec) -> Result<(), ExecutorValidationError> {
+        libcontainer::workload::default::get_executor().validate(spec)
+    }
+}
 
 #[tracing::instrument()]
 async fn pull_image(image: &str) -> Result<ImageData, Box<dyn std::error::Error>> {
@@ -34,9 +60,104 @@ async fn unpack_image(image_data: oci_distribution::client::ImageData) -> std::i
         let tar_gz = Cursor::new(layer.data);
         let tar = GzDecoder::new(tar_gz);
         let mut archive = Archive::new(tar);
-        archive.unpack("/tmp/image")?;
+        archive.unpack("/tmp/container/rootfs")?;
     }
     Ok(())
+}
+
+fn run_container() -> Result<(), Box<dyn std::error::Error>> {
+    let container_id = "my-container";
+    tracing::info!(container_id, "Creating container");
+    let mut container = ContainerBuilder::new(container_id.to_owned(), SyscallType::default())
+        .with_executor(MyExecutor {})
+        .with_pid_file(Some("/tmp/container/container.pid"))
+        .expect("invalid pid file")
+        // .with_console_socket(Some("/dev/stderr"))
+        .with_root_path("/tmp/container")
+        .expect("invalid root path")
+        .validate_id()?
+        .as_init("/tmp/container")
+        .with_systemd(false)
+        .with_detach(false)
+        .build()?;
+
+    tracing::info!(container_id, "Starting container");
+
+    container
+        .start()
+        .with_context(|| format!("failed to start container {}", container_id))?;
+
+    let _foreground_result = handle_foreground(container.pid().unwrap());
+
+    tracing::info!(container_id, "Deleting container");
+    container.delete(true)?;
+
+    Ok(())
+}
+
+#[tracing::instrument(level = "trace")]
+fn handle_foreground(init_pid: Pid) -> Result<i32> {
+    tracing::trace!("waiting for container init process to exit");
+    // We mask all signals here and forward most of the signals to the container
+    // init process.
+    let signal_set = SigSet::all();
+    signal_set
+        .thread_block()
+        .with_context(|| "failed to call pthread_sigmask")?;
+    loop {
+        match signal_set
+            .wait()
+            .with_context(|| "failed to call sigwait")?
+        {
+            signal::SIGCHLD => {
+                // Reap all child until either container init process exits or
+                // no more child to be reaped. Once the container init process
+                // exits we can then return.
+                tracing::trace!("reaping child processes");
+                loop {
+                    match waitpid(None, Some(WaitPidFlag::WNOHANG))? {
+                        WaitStatus::Exited(pid, status) => {
+                            if pid.eq(&init_pid) {
+                                return Ok(status);
+                            }
+
+                            // Else, some random child process exited, ignoring...
+                        }
+                        WaitStatus::Signaled(pid, signal, _) => {
+                            if pid.eq(&init_pid) {
+                                return Ok(signal as i32);
+                            }
+
+                            // Else, some random child process exited, ignoring...
+                        }
+                        WaitStatus::StillAlive => {
+                            // No more child to reap.
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            signal::SIGURG => {
+                // In `runc`, SIGURG is used by go runtime and should not be forwarded to
+                // the container process. Here, we just ignore the signal.
+            }
+            signal::SIGWINCH => {
+                // TODO: resize the terminal
+            }
+            signal => {
+                tracing::trace!(?signal, "forwarding signal");
+                // There is nothing we can do if we fail to forward the signal.
+                let _ = kill(init_pid, Some(signal)).map_err(|err| {
+                    tracing::warn!(
+                        ?err,
+                        ?signal,
+                        "failed to forward signal to container init process",
+                    );
+                });
+            }
+        }
+    }
 }
 
 #[tokio::main]
@@ -48,6 +169,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let image_data = pull_image("docker.io/library/alpine:latest").await?;
     unpack_image(image_data).await?;
+    run_container()?;
     Ok(())
 }
-
